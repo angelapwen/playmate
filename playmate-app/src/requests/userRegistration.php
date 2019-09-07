bb809bd91e6d79a7a36211f64759ef9f6b10872b
awengca <andrewweng@wustl.edu>
2019-09-07 15:10:55 -0400

    user registration

--- playmate-app/src/requests/userRegistration.php
+++ playmate-app/src/requests/userRegistration.php
@@ -1,5 +1,5 @@
 <?php
-    require_once '../phpFunctions/mySQL.php';
+    require_once '../phpFunctions/SQLworker.php';
     require_once '../phpFunctions/userManagement.php';
     
     header("Content-Type: application/json");
@@ -11,56 +11,14 @@
     $nickname = filterString($json_obj['nickname']);
     $password1 = $json_obj['password1'];
     $password2 = $json_obj['password2'];
-   
-    // Validate username check
-    if (!isValidUsername($username)) {
-        echo json_encode(array(
-            'success' => false,
-            'message' => 'Invalid username: Must consist of only alphanumeric characters and 1-50 characters in length.'
-        ));
-        exit();
-    }
-    // Validate nickname check
-    else if (!isValidNickname($nickname)) {
-        echo json_encode(array(
-            'success' => false,
-            'message' => 'Invalid nickname: Must consist of only alphanumeric characters and/or spaces and 1-50 characters in length.'
-        ));
-        exit();
-    }
-    // Validate password check
-    else if (!isValidPassword($password1)) {
-        echo json_encode(array(
-            'success' => false,
-            'message' => 'Invalid password: Must be between 1-75 (inclusive) characters in length.'
-        ));
-        exit();
-    }
-    // Validate passwords match
-    else if ($password1 !== $password2) {
-        echo json_encode(array(
-            'success' => false,
-            'message' => 'Passwords do not match. Please try again and type CAREFULLY.'
-        ));
-        exit();
-    }
-    // Validate username isn't already taken
-    else if (usernameIsTaken($username)) {
-        echo json_encode(array(
-            'success' => false,
-            'message' => 'This username has already been registered by another user. Please enter another appropriate username.'
-        ));  
-        exit();
-    }
-    // Validate nickname isn't already taken
-    else if (nicknameIsTaken($nickname)) {
-        echo json_encode(array(
-            'success' => false,
-            'message' => 'This nickname has already been registered by another user. Please enter another appropriate nickname.'
-        ));  
-        exit();
-    }
-    // All checks passed, commit the new user to the database
+
+    if ($password1 !== $password2) {
+             echo json_encode(array(
+                 'success' => false,
+                 'message' => 'Passwords do not match. Please try again and type CAREFULLY.'
+             ));
+             exit();
+         }
     else {
         commitUserToDatabase($username, $nickname, $password1);
         logInUser($username);  
@@ -70,3 +28,55 @@
         ));      
         exit();
     }
+
+   
+    // // Validate username check
+    // if (!isValidUsername($username)) {
+    //     echo json_encode(array(
+    //         'success' => false,
+    //         'message' => 'Invalid username: Must consist of only alphanumeric characters and 1-50 characters in length.'
+    //     ));
+    //     exit();
+    // }
+    // // Validate nickname check
+    // else if (!isValidNickname($nickname)) {
+    //     echo json_encode(array(
+    //         'success' => false,
+    //         'message' => 'Invalid nickname: Must consist of only alphanumeric characters and/or spaces and 1-50 characters in length.'
+    //     ));
+    //     exit();
+    // }
+    // // Validate password check
+    // else if (!isValidPassword($password1)) {
+    //     echo json_encode(array(
+    //         'success' => false,
+    //         'message' => 'Invalid password: Must be between 1-75 (inclusive) characters in length.'
+    //     ));
+    //     exit();
+    // }
+    // // Validate passwords match
+    // else if ($password1 !== $password2) {
+    //     echo json_encode(array(
+    //         'success' => false,
+    //         'message' => 'Passwords do not match. Please try again and type CAREFULLY.'
+    //     ));
+    //     exit();
+    // }
+    // // Validate username isn't already taken
+    // else if (usernameIsTaken($username)) {
+    //     echo json_encode(array(
+    //         'success' => false,
+    //         'message' => 'This username has already been registered by another user. Please enter another appropriate username.'
+    //     ));  
+    //     exit();
+    // }
+    // // Validate nickname isn't already taken
+    // else if (nicknameIsTaken($nickname)) {
+    //     echo json_encode(array(
+    //         'success' => false,
+    //         'message' => 'This nickname has already been registered by another user. Please enter another appropriate nickname.'
+    //     ));  
+    //     exit();
+    // }
+    // All checks passed, commit the new user to the database
+   
