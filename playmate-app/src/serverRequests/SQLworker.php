471441c5b8d63a45d0925d71acd388841acd7d2e
awengca <andrewweng@wustl.edu>
2019-09-07 14:00:29 -0400

    initialize sql worker

---
+++ playmate-app/src/serverRequests/SQLworker.php
@@ -0,0 +1,20 @@
+<?php
+    // Establishes a connection to the mySQL database
+    function connectMySQL() {
+        // Log into the mySQL database with credentials
+        $username = 'worker';
+        $password = 'worker';
+        $database = 'playmate';
+        $mySQLi = new mysqli('localhost', $username, $password, $database);
+        
+        // Returns false on a connection error
+        if ($mySQLi -> connect_errno) {
+            printf("Connection failed: %s\n", $mySQLi -> connect_error);
+            return false;
+        // Otherwise return the connection object
+        } else {
+            return $mySQLi;
+        }
+    }
+    ?>
+    
