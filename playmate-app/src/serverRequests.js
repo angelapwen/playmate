072db9e530bcc5a131d1824f377d4ad7ca37fff1
awengca <andrewweng@wustl.edu>
2019-09-07 14:36:09 -0400

    serverRequests dev

---
+++ playmate-app/src/serverRequests.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+// Submit the user registration information to the server
+function submitUserRegistration() {
+    // Package user inputs
+    const username = document.getElementById("register_usernameInput").value;
+    const nickname = document.getElementById("register_nicknameInput").value;
+    const password1 = document.getElementById("register_password1Input").value;
+    const password2 = document.getElementById("register_password2Input").value;
+    const data = {
+        "username": username,
+        "nickname": nickname,
+        "password1": password1,
+        "password2": password2
+    };
+        
+    fetch("requests/userRegistration.php", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "content-type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(data => response = data)
+    .then(() => document.getElementById("register_message").textContent = response.message)
+    .then(() => console.log(response.message))
+    .then(() => {
+        if (response.success) {
+    
+                //TODO
+        }
+    });
+}
